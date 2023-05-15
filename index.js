6755b48cac26d42272e362b8aaa190f576610600
paulaandrezza <paulaandrezza25@gmail.com>
2023-05-15 15:09:59 -0300

    Final project

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('olá mundo');
+import fs from "fs";
+import chalk from 'chalk';
+
+function trataErro(erro) {
+  throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório!'))
+}
+
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, "utf-8")
+    console.log(chalk.green(texto))
+  } catch (erro) {
+    trataErro(erro)
+  }
+}
+
+// promises com then()
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   fs.promises
+//     .readFile(caminhoDoArquivo, "utf-8")
+//     .then((texto) => console.log(chalk.green(texto)))
+//     .catch(trataErro)
+// }
+
+// function pegaArquivo(caminhoDoArquivo) {
+//   fs.readFile(caminhoDoArquivo, 'utf-8', (erro, texto) => {
+//     if (erro) {
+//       trataErro(erro)
+//     }
+//     console.log(chalk.green(texto));
+//   })
+// }
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/texto');
